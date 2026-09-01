112eb4cd3e80097be1b1b3bab2aaf678d0a7370d
Theresa Preza <prezatheresa1@gmail.com>
2026-09-01 13:41:14 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,5 @@
+#20 * only
+
+asterisk="\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t* \t\t*\n\t\t\t\t\t\t\t\t* \t\t\t\t*\n\t\t\t\t\t\t\t* \t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t my name is \t\t\t\t*\n \t\t\t\t\t*\t\t\t\t \"theresa preza\"\t\t\t\t*\n\t\t\t\t\t\t* \t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t* \t\t\t\t*\n\t\t\t\t\t\t\t\t\t* \t\t*\n\t\t\t\t\t\t\t\t\t\t*"
+
+print(asterisk)
