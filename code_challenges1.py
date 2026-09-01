bd405d56cb681b9624868135223307c301eb5122
Theresa Preza <prezatheresa1@gmail.com>
2026-09-01 13:41:54 +0800

    Delete code_challenges1.py

--- code_challenges1.py
+++
@@ -1,5 +0,0 @@
-#20 * only
-
-asterisk="\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t* \t\t*\n\t\t\t\t\t\t\t\t* \t\t\t\t*\n\t\t\t\t\t\t\t* \t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t\t my name is \t\t\t\t*\n \t\t\t\t\t*\t\t\t\t \"theresa preza\"\t\t\t\t*\n\t\t\t\t\t\t* \t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t* \t\t\t\t*\n\t\t\t\t\t\t\t\t\t* \t\t*\n\t\t\t\t\t\t\t\t\t\t*"
-
-print(asterisk)
