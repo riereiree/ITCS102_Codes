47bbd1d0339a5ef146ce09c785ddc946cd0b3281
Theresa Preza <prezatheresa1@gmail.com>
2026-09-03 09:52:03 +0800

    Update code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -1,14 +1,6 @@
 money = int(input("Enter amount to DEPOSIT -> "))
-#computation here
-b = 1000
-c = 500
-e = 200
-g = 100
-i = 50
-j = 20
-k = 10
-l = 5
-n = 1
+
+#computation here
 
 thousand = money // 1000
 five_hund = (money % 1000) // 500
@@ -21,13 +13,13 @@ five = (money % 1000 % 500 % 200 % 100 % 50 % 20 % 10) // 5
 one = (money % 1000 % 500 % 200 % 100 % 50 % 20 % 10 % 5) // 1
 
 #prints here
-print(b, "1000 = ",thousand)
-print(c, " 500 = ",five_hund)
-print(e, " 200 = ",two_hund)
-print(g, " 100 = ",one_hund)
-print(i, "  50 = ",fifty)
-print(j, "  20 = ",twenty)
-print(k, "  10 = ",ten)
-print(l, "   5 = ",five)
-print(n, "   1 = ",one)
+print("1000 = ",thousand)
+print(" 500 = ",five_hund)
+print(" 200 = ",two_hund)
+print(" 100 = ",one_hund)
+print("  50 = ",fifty)
+print("  20 = ",twenty)
+print("  10 = ",ten)
+print("   5 = ",five)
+print("   1 = ",one)
 print("The total money deposited are",money)
