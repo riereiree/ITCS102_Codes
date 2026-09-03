41e732974561d5455aa5d29e548a9150053b2895
Theresa Preza <prezatheresa1@gmail.com>
2026-09-03 10:54:03 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,19 @@
+# assignment operator 
+
+total = 0
+
+n1= float(input("Enter a number -> "))
+total = total + n1
+n2= eval(input("Enter a number -> "))
+total += n2
+n3= int(input("Enter a number ->"))
+total += n3
+n4= int(input("Enter a number ->"))
+total /= n4
+n5= int(input("Enter a number ->"))
+total -= n5
+n6= int(input("Enter a number ->"))
+total *= n6
+
+
+print("The total is -> ", total)
---
+++ activity8.py
@@ -0,0 +1,17 @@
+hobbies = " " #blank string
+
+hb = input("What are your hobbies? ")
+
+hobbies += hb + ",  "
+
+hb = input("What else? ")
+
+hobbies += hb + ",  "
+
+hb = input("What else? ")
+
+hobbies += hb
+
+hb = input("What else? ")
+
+print("My hobbies are ", hobbies)
