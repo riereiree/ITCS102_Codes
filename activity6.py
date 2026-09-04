b9d4906e1ccb44bc62e8a6e14ad963e440f99e8a
Theresa Preza <prezatheresa1@gmail.com>
2026-09-04 18:02:17 +0800

    Update activity6.py

--- activity6.py
+++ activity6.py
@@ -1,12 +1,8 @@
 # int(), eval(), float ()
 name = input("What's your name ? ")
-
 print("Hi," ,name, "How are you?")
 
-
 x = int(input("Enter a number -"))
-
 sum = x + 4
-
 print("Total = ", sum)
 print(type(x))
